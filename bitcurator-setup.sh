4adad8c9ad318ebfd4d748748f4d4d5c9db6e085
Tim Walsh <timothyryanwalsh@gmail.com>
2016-12-20 10:25:26 -0500

    Update bitcurator-setup.sh

--- bitcurator-setup.sh
+++ bitcurator-setup.sh
@@ -14,6 +14,8 @@ wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | 
 echo "deb http://dl.bintray.com/siegfried/debian wheezy main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update && sudo apt-get install siegfried
 
+cd /home/bcadmin
+
 # Install Brunnhilde
 echo "Installing Brunnhilde..."
 git clone https://github.com/timothyryanwalsh/brunnhilde
@@ -22,12 +24,15 @@ sudo mv brunnhilde/ /usr/share/
 # Install Brunnhilde GUI
 echo "Installing Brunnhilde GUI..."
 git clone https://github.com/timothyryanwalsh/brunnhilde-gui
-cd brunnhilde-gui && sudo bash install.sh
+cd /home/bcadmin/brunnhilde-gui
+sudo bash install.sh
 
 # Install CCA Tools
+cd /home/bcadmin
 echo "Installing CCA Tools..."
 git clone https://github.com/timothyryanwalsh/cca-tools
-cd cca-tools && sudo bash install_ccatools.sh
+cd cca-tools
+sudo bash install.sh
 
 # Remind user to update unhfs
 echo "Finished. ONE TASK REMAINING: YOU MUST STILL UPDATE HFSEXPLORER! Use this version: https://sourceforge.net/projects/catacombae/files/HFSExplorer/0.23.1%20%28snapshot%202016-09-02%29/"
