2213c1f284087104a1419a94e667843770a7f4c2
Tim Walsh <timothyryanwalsh@gmail.com>
2016-12-20 09:40:05 -0500

    Add files via upload

---
+++ bitcurator-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+### Bitcurator setup script
+# Tim Walsh - Dec 2017
+# Run as sudo
+
+# Install PyQt4
+echo "Installing PyQt4..."
+sudo apt-get install python-qt4
+
+# Install Siegfried
+echo "Installing Siegfried..."
+wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
+echo "deb http://dl.bintray.com/siegfried/debian wheezy main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update && sudo apt-get install siegfried
+
+# Install Brunnhilde
+echo "Installing Brunnhilde..."
+git clone https://github.com/timothyryanwalsh/brunnhilde
+sudo mv brunnhilde/ /usr/share/
+
+# Install Brunnhilde GUI
+echo "Installing Brunnhilde GUI..."
+git clone https://github.com/timothyryanwalsh/brunnhilde-gui
+cd brunnhilde-gui && sudo bash install.sh
+cd ..
+
+# Install CCA Tools
+echo "Installing CCA Tools..."
+git clone https://github.com/timothyryanwalsh/cca-tools
+cd cca-tools && sudo bash install_ccatools.sh
+
+# Remind user to update unhfs
+echo "Finished. ONE TASK REMAINING: YOU MUST STILL UPDATE HFSEXPLORER! Use this version: https://sourceforge.net/projects/catacombae/files/HFSExplorer/0.23.1%20%28snapshot%202016-09-02%29/"
