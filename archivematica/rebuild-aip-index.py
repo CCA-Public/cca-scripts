c7f35220d7475bb319ae854ed0be10b8d922cf6b
Tim Walsh <timothyryanwalsh@gmail.com>
2017-11-28 15:52:02 -0500

    Update rebuild-aip-index.py

--- archivematica/rebuild-aip-index.py
+++ archivematica/rebuild-aip-index.py
@@ -7,5 +7,5 @@ import subprocess
 aip_list = [] # Add each UUID to add to index as string in this list
 
 for aip in aip_list:
-    index_command = "sudo /home/twalsh/reindex-aip.sh %s" % aip
+    index_command = "sudo /home/<user>/reindex-aip.sh %s" % (aip)
     subprocess.call(index_command, shell=True)
