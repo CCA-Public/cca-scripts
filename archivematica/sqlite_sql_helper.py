1418b69c846f9a365bbd9c66fd6357dd10aed133
Tim Walsh <timothyryanwalsh@gmail.com>
2018-03-01 10:47:54 -0500

    Return response

--- archivematica/sqlite_sql_helper.py
+++ archivematica/sqlite_sql_helper.py
@@ -13,22 +13,24 @@ import sqlite3
 import sys
 
 def _make_parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("db", help="Path to SQLite file")
-	parser.add_argument("command", help="Command to run")
-	parser.add_argument("value", help="Value to use in command")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("db", help="Path to SQLite file")
+    parser.add_argument("command", help="Command to run")
+    parser.add_argument("value", help="Value to use in command")
 
-	return parser
+    return parser
 
 def main():
 
-	parser = _make_parser()
-	args = parser.parse_args()
+    parser = _make_parser()
+    args = parser.parse_args()
 
-	db = os.path.abspath(args.db)
-	conn = sqlite3.connect(db)
-	c = conn.cursor
+    db = os.path.abspath(args.db)
+    conn = sqlite3.connect(db)
+    c = conn.cursor
 
-	c.execute(args.command, (args.value,))
-	conn.commit()
-	c.close()
+    mydata = c.execute(args.command, (args.value,))
+    conn.commit()
+    c.close()
+
+    return mydata
